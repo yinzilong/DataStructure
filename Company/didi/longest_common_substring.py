0adb826a73ef09fbe743c1c8a3f8ef82ffee5b4c
yinzilong <1169256548@qq.com>
2018-06-17 11:53:26 +0800

    代码bug,更改了有关遍历下标的问题

--- Company/didi/longest_common_substring.py
+++ Company/didi/longest_common_substring.py
@@ -10,24 +10,21 @@
 """
 #声明二维数组也要注意，用列表生成式
 def getLongestCommonSubstring(str1, str2):
-    c = [[0 for j in range(len(str2))] for i in range(len(str1))]   #内循环i表示列，外循环j表示行
+    c = [[0 for j in range(len(str2)+1)] for i in range(len(str1)+1)]   #内循环i表示列，外循环j表示行,为了方便计算，必字符串长度多了一列
     for i in range(len(str1)):
         for j in range(len(str2)):
             if str1[i]==str2[j]:
-                if i==0 or j==0:    #必须要解决i==0 或者是 j==0的情况，因为后面要用到i-1 和 j-1
-                    c[i][j] = 1
-                else:
-                    c[i][j] = c[i-1][j-1] + 1
+                c[i+1][j+1] = c[i][j] + 1
             else:
-                c[i][j] = 0
+                c[i+1][j+1] = 0
     max_num = c[0][0]
     max_str1_endIndex = 0
-    for i in range(len(str1)):
-        for j in range(len(str2)):
+    for i in range(len(str1)+1):
+        for j in range(len(str2)+1):
             if c[i][j] > max_num:
                 max_num = c[i][j]
                 max_str1_endIndex = i
-    return max_num, str1[max_str1_endIndex-max_num+1:max_str1_endIndex+1]
+    return max_num, str1[max_str1_endIndex-max_num:max_str1_endIndex]
 
 if __name__=="__main__":
     str1 = "abcdefghijklmnopwa"
